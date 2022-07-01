11f54875fa75ebc45e997fa752806d33c5a25cb0
Widnie <widniedorilas890@yahoo.com>
2022-07-01 16:03:22 -0500

    Initial commits

---
+++ config.py
@@ -0,0 +1,25 @@
+# Dictionary file location, change if needed.
+dictionaryloc = 'Data TXT Files\dictionary.txt'
+
+#reading the dictionary 
+
+# Turn text file location, change if needed.
+turntextloc = 'DataFiles/turntext.txt'
+
+# Wheel contents for file location, change if needed.
+wheeltextloc = 'DataFiles/wheeldata.txt'
+
+# End of Round Status status file, change if needed.
+roundstatusloc = 'DataFiles/roundstatus.txt'
+
+# number of rounds in a game
+maxrounds = 3
+
+# Vowel cost
+vowelcost = 250
+
+# final prize, you fill this in
+finalprize = 10000
+
+# final round, change if needed.
+finalRoundTextLoc = 'DataFiles/finalround.txt'
